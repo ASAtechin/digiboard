3f2610189eb2c44b39e01d70d0a1376f88e50004
Your Name <your.email@example.com>
2025-09-16 14:43:36 +0530

    Fix Netlify build: use correct build script and update Flutter version

--- build-netlify.sh
+++ build-netlify.sh
@@ -6,8 +6,8 @@ echo "Installing Flutter for Netlify build..."
 
 # Download and install Flutter
 cd /opt/build
-wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.35.2-stable.tar.xz
-tar xf flutter_linux_3.35.2-stable.tar.xz
+wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
+tar xf flutter_linux_3.24.3-stable.tar.xz
 export PATH="/opt/build/flutter/bin:$PATH"
 
 # Verify Flutter installation
